603e82c9151eb8d9093ec467079f31f8779b26f7
Student <example@example.com>
2019-10-30 16:55:42 -0700

    Fixed piglatinator so that apples works

--- js/scriptsver2.js
+++ js/scriptsver2.js
@@ -4,25 +4,30 @@ function piglatinator (string) {
   var newWord = "";
   var quack = "";
 
+  // if (string.includes("/^[0-9]/*$")){
+  //   console.log("bad");
+  //   return "Eat a bag of bananas";
+  // }
+
   if (("a" || "e" || "i" || "o" || "u") === string.charAt(0) ){
     newWord = string + "way";
-    console.log(newWord);
   }
 
-  else if (("a" || "e" || "i" || "o" || "u") !== string.charAt(0) )
+  else if (("a" || "e" || "i" || "o" || "u") !== string.charAt(0) ) {
     for (i = 0; i < string.length; i++){
       if (("a" || "e" || "i" || "o" || "u") === string.charAt(i) && i > 0){
         newWord = string.slice(i) + string.slice(0, i);
       }
     }
     newWord += "ay";
-    console.log(newWord);
+  }
 
   if (string.includes("qu")) {
     quack = string.indexOf("qu");
     newWord = (string.slice(quack + 2)) + (string.slice(0, quack + 2)) + "ay";
     console.log(newWord);
   }
+  console.log(newWord);
   return newWord;
 }
 
@@ -33,6 +38,7 @@ $(document).ready(function(){
 
     var userInput = $("#userString").val();
     var latinatedString = piglatinator(userInput);
+
 
     $("#resultText").append(latinatedString);
     $(".resultBox").slideDown();
